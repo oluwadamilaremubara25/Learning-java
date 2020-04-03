6aa32c68b61aad7691b7edc2e04aa46913f7f6db
oluwadamilaremubara25 <oluwadamilaremubarak55@gmail.com>
2020-04-03 07:50:35 -0700

    commit

---
+++ BoolDemo.java
@@ -0,0 +1,12 @@
+//Demonstrate boolean values.
+class BoolDemo{public static void main (Strings args []) {
+    boolean b;
+
+    b = false;
+    System.out.println("b is" +b) ;
+    b= true ;
+    System.out.println("b is " +b) ;
+ 
+  }
+
+}
